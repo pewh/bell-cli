649d2a88781eece22971ab69ca946a4c7842a4e7
Ryan <25minutes.again@gmail.com>
2016-08-23 15:47:23 +0700

    Refactor by migrate to inquirer, fix npm version, remove unnecessary packages

--- index.js
+++ index.js
@@ -1,56 +1,91 @@
 #!/usr/bin/env node
 
-// params: email, subject
+'use strict';
 
-const config = require('home-config').load('.bellrc');
-const argv = require('yargs').argv;
+const homeConfig = require('home-config');
+const inquirer   = require('inquirer');
 
-const noConfig = () => !Object.keys(config.getAll()).length;
-const init     = argv._[0]
-const { mailgunKey, mailgunDomain, email, subject } = argv
+const config    = homeConfig.load('.bellrc');
+const noConfig  = !Object.keys(config.getAll()).length;
+const firstArgs = process.argv[2];
+const init      = (firstArgs === 'init');
 
-const defaultParamsMessage = `
+function _required (value) {
+  if (value) return true;
 
-Please run this command below:
-bell init --mailgun-key=<mailgun_key> --mailgun-domain=<mailgun_domain> --email=<email_address> --subject=<optional_subject>
-`
-
-if (init) {
-  config.section = Object.assign({},
-    mailgunKey ? { mailgunKey } : {},
-    mailgunDomain ? { mailgunDomain } : {},
-    email ? { email } : {},
-    subject ? { subject: subject || 'Bell' } : {}
-  )
-
-  if (!Object.keys(config.section).length) {
-    console.log('Params cannot be empty.', defaultParamsMessage)
-  }
-  else {
-    config.save()
-    console.log('Configuration has been saved.')
-  }
-
-  return
+  return 'Field is required';
 }
 
-if (noConfig()) {
-  console.log('No configuration found.', defaultParamsMessage)
-  return
+const questionsForInitConfig = [
+  { name: 'mailgunKey',
+    type: 'input',
+    message: 'Mailgun API Key',
+    validate: _required,
+  },
+  { name: 'mailgunDomain',
+    type: 'input',
+    message: 'Your custom Mailgun domain',
+    validate: _required,
+  },
+  { name: 'email',
+    type: 'input',
+    message: 'Default recipient email',
+    validate: _required,
+  },
+  { name: 'subject',
+    type: 'input',
+    message: 'Default email subject',
+    default: 'Bell',
+  },
+  { name: 'text',
+    type: 'input',
+    message: 'Default text of email',
+    default: 'Bell for you',
+  },
+];
+
+function initConfig () {
+  inquirer.prompt(questionsForInitConfig).then(answers => {
+    Object.keys(answers).forEach(param => {
+      config[param] = answers[param]
+    });
+
+    config.save();
+    console.log('Configuration has been saved.');
+  })
+  .catch(err => {
+    console.log('Error on init configuration.\n', err);
+  });
+}
+
+if (init) {
+  initConfig();
+  return;
+}
+
+if (noConfig) {
+  console.log('Please init configuration below.\n');
+  initConfig();
+  return;
 }
 
 const data = {
   from:    'Bell <hi@signalvip.com>',
-  to:      config.section.email,
-  text:    argv._[0] || 'Bell!',
-  subject: subject || 'Bell'
+  to:      config.email,
+  text:    firstArgs || config.text,
+  subject: config.subject,
 };
 
 const mailgun = require('mailgun-js')({
-  apiKey: config.section.mailgunKey,
-  domain: config.section.mailgunDomain,
+  apiKey: config.mailgunKey,
+  domain: config.mailgunDomain,
 });
 
 mailgun.messages().send(data, (error, body) => {
-  console.log(body);
+  if (error) {
+    console.log('Something is wrong.', error);
+  }
+  else {
+    console.log('You should get a bell!', body);
+  }
 });
